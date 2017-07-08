815b3104076872f64a2422fc9373eba2a0774baa
Dmytro <ddorotiuk@gmail.com>
2017-07-08 10:46:58 +0200

    Add grunt-sass to gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -4,12 +4,24 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: ["js/*.js"]
+    },
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.sass'
+        }
+      }
     }
   });
     
   // Load the plugins tasks
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-sass");
 
   // Default task(s).
-  grunt.registerTask("default", ["jshint"]);
+  grunt.registerTask("default", ["jshint", "sass"]);
 };
